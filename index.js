16ed6bd9d934f8660e7482b1f7c6d8712020f00d
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-08-05 10:15:42 +0530

    boiler-plate

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+dotenv.config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("DB connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
